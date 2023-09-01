b666c35c2570187fa741fd51f96bf04c5bf0651a
houssni-amin <houssni.amin@outlook.fr>
2023-09-01 16:48:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Etape 1 - Sélectionner nos éléments
+let input = document.querySelector('#prix')
+let error = document.querySelector('small')
+let formulaire = document.querySelector('#formulaire')
+
+// Etape 2 - Cacher l'erreur
+error.style.display = 'none'
+
+// Etape 3 - Générer un nombre aléatoire
+let nombreAleatoire = Math.floor(Math.random() * 1001)
+let coup = 0
+let nombreChoisi
+
+// Etape 6 - Créer la fonction vérifier
+function verifier(nombre) {
+    let instructuion = document.createElement('div')
+
+    if (nombre < nombreAleatoire ) {
+        instructuion.textContent = `#${coup} (${nombre}) C'est plus !`
+        instructuion.className = 'instruction plus'
+    }else if (nombre > nombreAleatoire) {
+        instructuion.textContent = `#${coup} (${nombre}) C'est moins !`
+        instructuion.className = 'instruction moins'
+    }else{
+        instructuion.textContent = `#${coup} (${nombre}) Félicitation vous avez trouvé le juste prix !`
+        instructuion.className = 'instruction fini'
+        input.disabled = true
+    }
+    document.querySelector('#instructions').prepend(instructuion)
+
+}
+// Etape 4 - Vérifier que l'utilisateur donne bien un nombre
+input.addEventListener('keyup', () => {
+    if (isNaN(input.value)) {
+        error.style.display = 'inline'
+    }else{
+        error.style.display = 'none'
+    }
+})
+
+// Etape 5 - Agir à l'envoi du formulaire*
+formulaire.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (isNaN(input.value) || input.value == "") {
+        input.style.borderColor = 'red'
+    }else{
+        coup++
+        input.style.borderColor = 'silver'
+        nombreChoisi = input.value
+        input.value = ""
+        verifier(nombreChoisi)
+    }
+})
